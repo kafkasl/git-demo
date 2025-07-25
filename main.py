c4e8182b29c904ea73baca6f9a491f6ea0c0845e
Pol Alvarez <pol.avms@gmail.com>
2025-07-25 19:36:05 +0200

    basic oauthy

--- main.py
+++ main.py
@@ -1,23 +1,50 @@
 from fasthtml.common import *
+from fasthtml.oauth import GoogleAppClient, OAuth
+from monsterui.all import *
+
+
+cli = GoogleAppClient(client_id='YOUR_KEY_HERE',
+                      client_secret='YOUR_SECRET_HERE')
+
+class Auth(OAuth):
+    def get_auth(self, info, ident, session, state):
+        email = info.email or ''
+        session['email'] = email
+
+        return RedirectResponse('/', status_code=303)
 
 hdrs = (
     Link(rel='stylesheet', href='style.css', type='text/css'),
-    # Script("htmx.logAll();") # debug HTMX events
 )
+skip = ('/login', '/logout', '/redirect', r'/.*\.(png|jpg|ico|css|js|md|svg)', '/static')
 
 app, rt = fast_app(hdrs=hdrs)
+oauth = Auth(app, cli, skip=skip)
 
-@rt("/")
-def get():
-    return Titled("FastHTML Demo - Git Workshop",
-        P("Welcome to FastHTML!"),
-        P("This is NOT simple demo for the Git workshop."),
-        Button("Click me!", hx_post="/clicked", hx_target="#result"),
-        Div(id="result")
+
+@app.get
+def index(session):
+    email = session.get('email', None)
+
+    return (Title("Git 4 FastHTML"), P(f"Welcome {email}"),)
+
+@app.get('/login')
+def login(req): 
+    return (
+        Title("Git 4 FastHTML"),
+        DivVStacked(
+            Img(src="https://icons-8e9.pages.dev/favicon-black.svg", width=100),
+            DivVStacked(
+                H1('Git 4 FastHTML', cls="text-center"),
+                P("Track your changes and move fast", cls="text-center"),
+                A(Button("Log in with Google"), href=oauth.login_link(req))
+            ),
+            cls="pt-[20vh]",
+        ),
     )
 
-@rt("/clicked")
-def post():
-    return P("You clicked the button! 🎉", cls="text-green-600")
+ 
 
-serve()
+# Start server
+if __name__ == "__main__":
+    serve()
